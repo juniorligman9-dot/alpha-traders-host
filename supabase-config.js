db43c91118d9476e4a4c70826d297e40fb7ce566
juniorligman9-dot <juniorligman9@gmail.com>
2026-09-02 18:15:23 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,8 @@
-const SUPABASE_URL = "https://oavqwluxiqqfzvdzdrzl.supabase.co";
+const SUPABASE_URL = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 
-const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_T7FLuaPz21XX4Bu73BSWXg_zFfXPFYW";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_T7FLuaPz21XX4Bu73BSWXg_zFfXPFYW";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
