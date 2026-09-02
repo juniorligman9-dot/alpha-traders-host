1896e3883061f8c29f5205c58a01c8f0cf54a0cb
juniorligman9-dot <juniorligman9@gmail.com>
2026-09-02 18:40:00 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+const SUPABASE_URL = "https://oavqwluxiqqfzvdzdrzl.supabase.co/rest/v1/";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_T7FLuaPz21XX4Bu73BSWXg_zFfXPFYW";
 
